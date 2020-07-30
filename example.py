072daf809c87589ce417e7acaa7613b0fd2ccea3
Engman Anton <Anton.Engman@uddeholm.com>
2020-07-30 13:34:44 +0200

    Update visual look of printed PDU

--- example.py
+++ example.py
@@ -2,13 +2,16 @@ from src import S7RequestBuilder
 
 def PrintSignals(signals):
 	for s in signals:
-		print(str(s.regionLetterCode) + str(s.db) + "." + str(s.offset) + ":" + str(s.datatype))
+		print(str(s.regionLetterCode) + str(s.db) + "." + str(s.offset) + ":" + str(s.datatype) + "\t\t\t.")
 
-signals = S7ResponseBuilder.RequestBuilder("test")
+signals = S7RequestBuilder.RequestBuilder("address.txt")
 
 print("###############################")
 for s in signals:
-	print("....................PDU......" + str(GetPDUSize(s)) + " Bytes(including item header)......." + str(len(s)) + " items.........")
+	i = 0
+	print("....................PDU......" + str(S7RequestBuilder.GetPDUSize(s)) + " Bytes(including item header)......." + str(len(s)) + " items.........")
 	for sig in s:
-		print("--------------")
+		i = i + 1
+		print("----------ITEM " + str(i) + "---------------.")
 		PrintSignals(sig)
+		print("--------------------------------.")
